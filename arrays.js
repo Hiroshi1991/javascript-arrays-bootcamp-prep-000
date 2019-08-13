a7d9fff20ae7b3293d483bb8ec9f6f8ffcba18cb
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-13 23:14:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,39 +1,3 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
-
-var addElementToBeginningOfArray= function(array, element) {
-    return [element,...array]
-    // array.unshift(element)
-    // return array
-}
-
-var destructivelyAddElementToBeginningOfArray= function(array, element) {
-    array.unshift(element)
-    return array
-}
-
-var addElementToEndOfArray= function(array, element) {
-    return [...array, element]
-    array.push(element)
-    return array
-}
-
-var destructivelyAddElementToEndOfArray= function(array, element) {
-    array.push(element)
-    return array
-}
-
-var accessElementInArray = function(array, index) {
-    return array[index]
-}
-
-var removeElementFromBeginningOfArray = function(array) {
-    return array.slice(1)
-}
-
-var removeElementFromEndOfArray = function(array) {
-    return array.slice(0, array.length - 1)
-}
-
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
@@ -76,7 +40,3 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop(...array);
-  return array
-}
